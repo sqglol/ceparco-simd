15071ffa3f30060efa29ecc5226ce679f8b4ac56
penguinforest <paulograne.c.silva@gmail.com>
2025-10-10 18:10:15 +0800

    ymm registers simd

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 // x86-64 implementation, no SIMD
 extern void x86_64_max(int n, float A[], float B[], float C[], int idx[]);
+extern void ymm_max(int n, float A[], float B[], float C[], int idx[]);
 
 // Prints the results of the program execution
 // TODO: First and last five only
@@ -32,11 +33,11 @@ void C_max(int n, float A[], float B[], float C[], int idx[]) {
 }
 
 int main() {
-    int n = 5;
-    float A[5] = {4, 3, 5, 8, 12};
-    float B[5] = {2, 4, 4, 8, 69};
-    float C[5] = {0};
-    int idx[5] = {0};
+    int n = 17;
+    float A[17] = {4, 3, 5, 8, 12, 20, 19, -5, -3, -2, 1, 2, 3, 4, 5, 6, 7};
+    float B[17] = {2, 4, 4, 7, 69, 37, 5, 0, -1, -3, 2, 9, 8, 21.5, 1, 0, 9};
+    float C[17] = {0};
+    int idx[17] = {0};
 
     clock_t start_time, end_time;
 
@@ -54,7 +55,15 @@ int main() {
     end_time = clock();
 
     print_results(n, C, idx);
-    printf("%f\n\n", (double) (end_time - start_time) / CLOCKS_PER_SEC);
+    printf("No SIMD: %f\n\n", (double) (end_time - start_time) / CLOCKS_PER_SEC);
+
+    // x86-64 with YMM, SIMD
+    start_time = clock();
+    ymm_max(n, A, B, C, idx);
+    end_time = clock();
+
+    print_results(n, C, idx);
+    printf("YMM, SIMD: %f\n\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
 
     return 0;
 }
